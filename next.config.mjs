570c44079b6f6248621d4b30399f823f9f1bb6cf
Rosegoldcruz <vulpine444@gmail.com>
2025-09-26 04:24:10 -0700

    video bg

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/the-lab",
+        destination: "/lab",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
